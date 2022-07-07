735da64521f275b37925f72085c8c1574c8ec748
quyen nguyen <joe.nguyen@transwap.com>
2022-07-07 13:48:03 +0700

    fix missing fi

--- autotest.sh
+++ autotest.sh
@@ -5,6 +5,7 @@ elif echo $OUTPUT | grep -q "Ubuntu 20.04" ; then
     apt install -y -qq wget curl
 elif echo $OUTPUT | grep -q "Ubuntu 22.04"; then
     apt install -y -qq wget curl 
+fi
 echo "Server release: $OUTPUT"
 #Download the install master script
 
